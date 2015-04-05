9f824947d72e45f7b8be3c54ec9a2437f85466ad
richelbilderbeek <richel@richelbilderbeek.nl>
2015-04-05 20:07:40 +0200

    Got one digit ticking

---
+++ MoreOrLessClock/MoreOrLessClock.ino
@@ -0,0 +1,120 @@
+/*
+  Shift Register SN74HC595 voorbeeldcode 1
+  (C) 2015 Richel Bilderbeek
+
+  Aangepast van onderstaand:
+*/
+/*
+SparkFun Inventor's Kit Galileo
+Example sketch 13
+
+SHIFT REGISTER
+
+This sketch was written by SparkFun Electronics,
+with lots of help from the Arduino community.
+This code is completely free for any use.
+Visit http://learn.sparkfun.com/products/2 for SIK information.
+Visit http://www.arduino.cc to learn about the Arduino.
+
+Version 2.0 6/2012 MDG
+*/
+/*
+
+Omschrijving chip en verbindingen:
+
+ 16 15 14 13 12 11 10 9
+ |  |  |  |  |  |  |  |
+ +--+--+--+--+--+--+--+
+ |>                   |
+ +--+--+--+--+--+--+--+
+ |  |  |  |  |  |  |  |
+ 1  2  3  4  5  6  7  8
+
+ Verbindingen:
+
+ 1: LED 2
+ 2: LED 3
+ 3: LED 4
+ 3: LED 5
+ 5: LED 6
+ 6: LED 7
+ 7: LED 8 
+ 8: GND
+ 9: wordt niet gebruikt
+ 10: naar 5V
+ 11: naar D4: clock pin
+ 12: naar D3: latch pin
+ 13: naar GND
+ 14: naar D2: data pin
+ 15: naar LED1
+ 16: naar 5V
+
+ Officiele pin namen:
+
+ 1: Q2
+ 2: Q3
+ 3: Q4
+ 3: Q5
+ 5: Q6
+ 6: Q7
+ 7: Q8 
+ 8: GND
+ 9: QH* (wordt niet gebruikt)
+ 10: SRCLR* (naar 5V)
+ 11: SRCLK (naar D4): clock pin
+ 12: RCLK (naar D3): latch pin
+ 13: OE* (naar GND)
+ 14: SER (naar D2): data pin
+ 15: Q1
+ 16: VCC (naar 5V)
+
+*/
+
+const int datapin  = 2; 
+const int latchpin = 3;
+const int clockpin = 4;
+
+void setup()
+{
+  pinMode(datapin,  OUTPUT);
+  pinMode(latchpin, OUTPUT);
+  pinMode(clockpin, OUTPUT);  
+}
+
+void loop()
+{
+  for (int i=0; i!=10; ++i)  
+  {
+    ShowBinary(~DigitToBinary(i));
+    delay(1000);
+  }
+}
+
+int DigitToBinary(const int digit)
+{
+  switch (digit)
+  {
+    case 0: return B11111100;
+    case 1: return B01100000;
+    case 2: return B11011010;
+    case 3: return B11110010;
+    case 4: return B01100110;
+    case 5: return B10110110;
+    case 6: return B10111110;
+    case 7: return B11100000;
+    case 8: return B11111110;
+    case 9: return B11110110;
+  } 
+
+}
+
+void ShowBinary(const int getal)
+{
+  // Stuur de data naar het shift register
+  shiftOut(datapin, clockpin, LSBFIRST, getal);
+
+  // Zet de latch aan en uit, zodat de outputs aan gaan
+  digitalWrite(latchpin, HIGH);
+  digitalWrite(latchpin, LOW);
+}
+
